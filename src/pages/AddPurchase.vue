fd811cc2d7e0e18c61d550aca0dc1748077fcc7b
arocebd <arocebd@gmail.com>
2025-11-28 08:40:10 +0600

    Change API import from api.js to axios

--- src/pages/AddPurchase.vue
+++ src/pages/AddPurchase.vue
@@ -180,7 +180,7 @@
 <script setup>
 import { ref, computed, onMounted } from "vue";
 import { useRoute, useRouter } from "vue-router";
-import api from "@/services/api.js";
+import api from "@/axios";
 
 const route = useRoute();
 const router = useRouter();
