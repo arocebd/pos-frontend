e6ddd54db95b98aff0b452a433fd7fb7967ac006
arocebd <arocebd@gmail.com>
2026-01-13 23:47:12 +0600

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://163.227.239.93/api/",   // Django backend
+  baseURL: "https://163.227.239.93/api/",   // Django backend
 });
 
 export default api;
